172d987c4f34f27195d00110d3a643b22dce3680
Frida Henriksen <fridahenriksen@hotmail.com>
2024-07-19 14:44:41 +0200

    Input and a list

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Createtodo = () => {
-  let tasks = 0;
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const tasks = ["Task 1", "Task 2", "Task 3"];
 
   const handleClick = () => {
-    tasks++;
-    console.log("Add Task", tasks);
+    setCount(count + 1);
   };
 
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
   return (
     <>
-      <h1>Tasks: {tasks} </h1>
+      <h1>Tasks: {count} </h1>
+      <input type="text" onChange={handleChange} />
       <button onClick={handleClick} value="Add Task Button">
         Add Task
       </button>
+      <h1>Input: {input}</h1>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task}>{task}</li>
+        ))}
+      </ul>
     </>
   );
 };
