e1f782209792cebeae01c7b3e51cb99477217b34
Frida Henriksen <fridahenriksen@hotmail.com>
2024-07-26 21:42:30 +0200

    Added tag component

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -14,10 +14,10 @@ const TaskForm = () => {
 
         <div className="task_form_bottom_line">
           <div>
-            <button className="tag">HTML</button>
-            <button className="tag">CSS</button>
-            <button className="tag">JavaScript</button>
-            <button className="tag">React</button>
+            <Tag />
+            <Tag />
+            <Tag />
+            <Tag />
           </div>
 
           <div>
