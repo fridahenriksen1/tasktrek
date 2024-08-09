18c5adf8685989932ccc8983a7c74ffe39c87716
Frida Henriksen <fridahenriksen@hotmail.com>
2024-08-09 19:30:48 +0200

    title names

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import "./App.css";
 import TaskForm from "./components/TaskForm";
 import TaskColumn from "./components/TaskColumn";
+import todoIcon from "./assets/direct-hit.png";
+import doingIcon from "./assets/glowing-star.png";
+import doneIcon from "./assets/check-mark-button.png";
 
 const App = () => {
   return (
     <div className="app">
       <TaskForm />
       <main className="app_main">
-        <TaskColumn title="To do" />
-        <TaskColumn title="Doing" />
-        <TaskColumn title="Done" />
+        <TaskColumn title="To do" icon={todoIcon} />
+        <TaskColumn title="Doing" icon={doingIcon} />
+        <TaskColumn title="Done" icon={doneIcon} />
       </main>
     </div>
   );
--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import Todo from "../assets/direct-hit.png";
+// import Todo from "../assets/direct-hit.png";
 
 import "./TaskColumn.css";
 
-const TaskColumn = (props) => {
+const TaskColumn = (title, icon) => {
+  //   const { title, icon } = props;
   return (
     <section className="task_Column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={Todo} alt="" />
-        {props.title}
+        <img className="task_column_icon" src={icon} alt="" />
+        {title}
       </h2>
     </section>
   );
