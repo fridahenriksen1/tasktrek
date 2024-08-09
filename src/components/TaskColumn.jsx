dcb22dc7bbceb332a2405b96a006da7ea7649b9a
Frida Henriksen <fridahenriksen@hotmail.com>
2024-08-09 20:13:20 +0200

    Added style to task card

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -2,15 +2,17 @@ import React from "react";
 // import Todo from "../assets/direct-hit.png";
 
 import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
 
-const TaskColumn = (title, icon) => {
+const TaskColumn = (props) => {
   //   const { title, icon } = props;
   return (
     <section className="task_Column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={icon} alt="" />
-        {title}
+        <img className="task_column_icon" src={props.icon} alt="" />
+        {props.title}
       </h2>
+      <TaskCard />
     </section>
   );
 };
