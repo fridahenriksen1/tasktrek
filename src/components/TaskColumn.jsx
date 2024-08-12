34e244c86256cfaa364d3a72b19d99c3d4da2a6e
Frida Henriksen <fridahenriksen@hotmail.com>
2024-08-12 15:26:16 +0200

    removed props

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -4,13 +4,12 @@ import React from "react";
 import "./TaskColumn.css";
 import TaskCard from "./TaskCard";
 
-const TaskColumn = (props) => {
-  //   const { title, icon } = props;
+const TaskColumn = ({ title, icon }) => {
   return (
     <section className="task_Column">
       <h2 className="task_column_heading">
-        <img className="task_column_icon" src={props.icon} alt="" />
-        {props.title}
+        <img className="task_column_icon" src={icon} alt="" />
+        {title}
       </h2>
       <TaskCard />
     </section>
