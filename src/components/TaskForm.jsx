47ac9f641020357dae5d4dfc6b4fab977ee433a1
Frida Henriksen <fridahenriksen@hotmail.com>
2024-08-12 15:57:59 +0200

    onChange functions

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./TaskForm.css";
 import "./Tag.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  const [task, setTask] = useState("");
+  const [status, setStatus] = useState("to do");
+
+  const handleTaskChange = (e) => {
+    setTask(e.target.value);
+  };
+  const handleStatusChange = (e) => {
+    setStatus(e.target.value);
+  };
+
+  console.log(task, status);
   return (
     <header className="app_header">
       <form>
@@ -12,6 +23,7 @@ const TaskForm = () => {
           type="text"
           className="task_input"
           placeholder="Enter your task"
+          onChange={handleTaskChange}
         />
 
         <div className="task_form_bottom_line">
@@ -23,7 +35,7 @@ const TaskForm = () => {
           </div>
 
           <div>
-            <select className="task_status">
+            <select className="task_status" onChange={handleStatusChange}>
               <option value="todo">To do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
