e594a042dc18d0fa822d04f16f2466eb42f6e3dc
Frida Henriksen <fridahenriksen@hotmail.com>
2025-11-18 17:44:58 +0100

    Added delete function

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,35 @@ import doneIcon from "./assets/check-mark-button.png";
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
-  console.log("tasks", tasks);
+  const handleDelete = (taskIndex) => {
+    const newTasks = tasks.filter((task, index) => index !== taskIndex);
+    setTasks(newTasks);
+  };
   return (
     <div className="app">
       <TaskForm setTasks={setTasks} />
       <main className="app_main">
-        <TaskColumn title="To do" icon={todoIcon} tasks={tasks} status="todo" />
+        <TaskColumn
+          title="To do"
+          icon={todoIcon}
+          tasks={tasks}
+          status="todo"
+          handleDelete={handleDelete}
+        />
         <TaskColumn
           title="Doing"
           icon={doingIcon}
           tasks={tasks}
           status="doing"
+          handleDelete={handleDelete}
         />
-        <TaskColumn title="Done" icon={doneIcon} tasks={tasks} status="done" />
+        <TaskColumn
+          title="Done"
+          icon={doneIcon}
+          tasks={tasks}
+          status="done"
+          handleDelete={handleDelete}
+        />
       </main>
     </div>
   );
